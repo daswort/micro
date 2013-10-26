61ad2431768326d72f5161c1c2904833f7249798
daswort <daswort@valhalla>
2013-10-26 10:49:11 -0300

    iniciando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Micro::Application.config.secret_key_base = '6e857a4c34a651c09c7944e898cb13cc9ef644a493bc1cb12362fe87732cb0baaf514c30f97b4a74d054a891bf6146f8932b3e7c9bb6dfd110af5909402e303a'
